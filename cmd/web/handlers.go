d87cb8e761313e3e7b7dbe42543ddb1a7ad987d8
Ian Cruz <iankencruz@gmail.com>
2024-01-11 01:02:18 +1100

    clean url

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,41 +3,39 @@ package main
 import (
 	"errors"
 	"fmt"
-
 	"net/http"
 	"strconv"
 
 	"github.com/iankencruz/snippetbox/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
+	// Because httprouter matches the "/" path exactly, we can now remove the
+	// manual check of r.URL.Path != "/" from this handler.
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
-
 	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
-
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// When httprouter is parsing a request, the values of any named parameters
+	// will be stored in the request context. We'll talk about request context
+	// in detail later in the book, but for now it's enough to know that you can
+	// use the ParamsFromContext() function to retrieve a slice containing these
+	// parameter names and values like so:
+	params := httprouter.ParamsFromContext(r.Context())
+	// We can then use the ByName() method to get the value of the "id" named
+	// parameter from the slice and validate it as normal.
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
-
-	// Use the SnippetModel's Get() method to retrieve the data for a
-	// specific record based on its ID. If no matching record is found,
-	// return a 404 Not Found response.
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
@@ -47,30 +45,29 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
-
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
-
 }
 
+// Add a new snippetCreate handler, which for now returns a placeholder
+// response. We'll update this shortly to show a HTML form.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
 
-	title := "0 snail"
-	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+// Rename this handler to snippetCreatePost.
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// Checking if the request method is a POST is now superfluous and can be
+	// removed, because this is done automatically by httprouter.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := 7
-
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	// Update the redirect path to use the new clean URL format.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/julienschmidt/httprouter" // New import
 	"github.com/justinas/alice"
+	"net/http"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	// Initialize the router.
+	router := httprouter.New()
 
+	// Update the pattern for the route for the static files.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	// And then create the routes using the appropriate methods, patterns and
+	// handlers.
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
+	// Create the middleware chain as normal.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
-	// Return the 'standard' middleware chain followed by the servemux.
-	return standard.Then(mux)
+	// Wrap the router with the middleware and return it as normal.
+	return standard.Then(router)
 }
